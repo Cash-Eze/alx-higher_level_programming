d552d9aa652d7cf071a7fccd081f243c4f1b0039
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-12-07 16:36:25 +0000

    sysargv

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,14 @@
-#/usr/bin/python3
-
+#!/usr/bin/python3
 import sys
 
-if __name__ != "__main__"
+if __name__ != "__main__":
     exit()
 
 i = 0
 result = 0
 for argument in sys.argv:
     if i != 0:
-        result = result + int(argument)
+        result += int(argument)
     else:
         i += 1
 print("{:d}".format(result))
