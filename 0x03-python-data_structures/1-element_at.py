7c0ca04c2c3e7d3062e5c9d6e7d8af9be8bda623
Amos Hiamatsu <amoshiamatsu@gmail.com>
2022-12-08 17:32:31 +0000

    list at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return 
-    elif idx > len(my_list) -1:
-        return
-    else:
-        return (my_list[idx])
-
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
