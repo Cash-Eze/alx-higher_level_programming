6eb40045e00ae663f3b0e963e5f8c40ccacab9a8
Amos Hiamatsu <amoshiamatsu@gmail.com>
2023-01-13 23:21:00 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" This module defines the read_file function"""
+"""This module defines the read_file function"""
 
-def read_file(filename="")
-    """Reads a text file and print it to the stdout
 
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout
     Args:
-    filename(str): Filename
+    filename (str): Filename
     """
-    with open(filename, encoding = 'utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         for line in file:
             print(line, end="")
