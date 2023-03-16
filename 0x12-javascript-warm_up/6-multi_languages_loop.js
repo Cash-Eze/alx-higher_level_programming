988040abab10206edf0acb5effd74ae7b726a837
Amos Hiamatsu <amoshiamatsu@gmail.com>
2023-03-15 18:26:05 -0700

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i;
-for (i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
+for (const string of strings) {
+  console.log(string);
 }
