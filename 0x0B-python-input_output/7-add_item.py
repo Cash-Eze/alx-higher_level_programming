7ccc0f5b5aa1bac1e8a45f19af27934e4c916328
Cash-Eze <amoshiamatsu@gmail.com>
2023-10-31 09:48:34 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """This module creates a script that adds args to Python list and saves them"""
 
-from os import path
-from sys import argv
+
+import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+
+try:
+    new_list = load_from_json_file('add_item.json')
+except:
+    new_list = []
+
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
+
+try:
+    save_to_json_file(new_list, 'add_item.json')
+except:
+    pass
